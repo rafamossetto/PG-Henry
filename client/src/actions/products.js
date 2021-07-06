35c6ee8d220b446c6f6a7b49115bc913b2731651
Mauro Haidr <maurohaidr@gmail.com>
2021-07-06 11:42:51 -0300

    Update Products.js

--- client/src/actions/products.js
+++ client/src/actions/products.js
@@ -1,24 +1,10 @@
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> 6470e41cab4246fddf997183b39d09ee7a67015d
 import axios from 'axios'
 
 export function getProducts() {
       return async function(dispatch) {
-<<<<<<< HEAD
         const result = await axios.get("http://localhost:3001/products");
-=======
-        const result = await axios.get("ruta de productos");
->>>>>>> 6470e41cab4246fddf997183b39d09ee7a67015d
         dispatch({ type: GET_PRODUCTS, payload: result.data });
       };
 }
 
-<<<<<<< HEAD
 export const GET_PRODUCTS = 'GET_PRODUCTS'
->>>>>>> Stashed changes
-=======
-export const GET_PRODUCTS = 'GET_PRODUCTS'
->>>>>>> 6470e41cab4246fddf997183b39d09ee7a67015d
