f6cd84b64103859f193e733d642577874e3e29de
Mauro Haidr <maurohaidr@gmail.com>
2021-07-06 08:41:36 -0300

    Added store/reducer/foldiers
    Added reducer files/foldier.
    Added store files/foldier.
    Added components foldier.
    Added details/Nav/Home/Products subfoldiers in components.
    Added/imported BrowserRouter/Provider/store to index.js

---
+++ client/src/components/products/Products.js
@@ -0,0 +1,6 @@
+import { Link } from 'react-router-dom';
+import{ } from '../actions/actions'
+import React, { useState, useEffect } from 'react';
+import { connect } from "react-redux";
+
+const Products = (props) => {}
