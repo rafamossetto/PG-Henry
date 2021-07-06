50bd25bc406633036f167b2846bbad49584c0dda
Mauro Haidr <maurohaidr@gmail.com>
2021-07-06 09:43:51 -0300

    products actions/reducer
    Added an action to get products.
    Added the case GET_PRODUCTS in reducer.
    Added state 'products' in initial state.

--- client/src/components/products/Products.js
+++ client/src/components/products/Products.js
@@ -1,6 +1,67 @@
 import { Link } from 'react-router-dom';
-import{ } from '../actions/actions'
 import React, { useState, useEffect } from 'react';
 import { connect } from "react-redux";
 
-const Products = (props) => {}
+const Products = (props) => {
+    
+
+    return(
+        <div>
+
+            <div> 
+                <div>
+                    <h3>Movie Title</h3>
+                    <p>field</p>
+                    <p>Time</p>
+                    <p>Date</p>
+                    <p>Price</p>
+                </div>
+                <div>Parking Lot</div>
+            </div>
+
+            <div>
+                <div>
+                    <p>Combos</p>
+                </div>
+                <div>
+                    <p>Combos mapping</p>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <p>Extras</p>
+                </div>
+                <div>
+                    <p>Extras mapping</p>
+                </div>
+            </div>
+
+            <div>
+                <p>* You can choose sweet or salty once you get there!</p>
+                <div>
+                    <p>Total: XXX</p>
+                    <button>Buy</button>
+                </div>
+            </div>
+ 
+        </div>
+    )
+}
+
+function mapStateToProps(state) {
+    return {
+
+    };
+  }
+  
+function mapDispatchToProps(dispatch) {
+    return {
+
+    };
+}
+  
+  export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Products);
