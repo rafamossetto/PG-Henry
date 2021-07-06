f7afcbd95336c44ffdf33627653b68bccf2e252b
Mauro Haidr <maurohaidr@gmail.com>
2021-07-06 11:03:29 -0300

    Added product action

---
+++ client/src/actions/products.js
@@ -0,0 +1,13 @@
+<<<<<<< Updated upstream
+=======
+import axios from 'axios'
+
+export function getProducts() {
+      return async function(dispatch) {
+        const result = await axios.get("http://localhost:3001/products");
+        dispatch({ type: GET_PRODUCTS, payload: result.data });
+      };
+}
+
+export const GET_PRODUCTS = 'GET_PRODUCTS'
+>>>>>>> Stashed changes
