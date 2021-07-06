24e194a442cd44682fefe6b6de5b002f07b1c85d
Gianfranco <70702029+Yiftleh@users.noreply.github.com>
2021-07-06 18:56:02 -0300

    user signup form created

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import { Route } from 'react-router';
 import Products from './components/products/Products'
+import SignupForm from './components/user/SignupForm'
 function App() {
   return (
     <div className="App">
       <Route path='/products' component={Products}/>
+      <Route path='/signup' component={SignupForm}/>
     </div>
   );
 }
