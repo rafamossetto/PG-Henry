311e54a535e45c9537d53a3af510832d28f56cfc
juancereceda <juanmcereceda@gmail.com>
2021-07-06 00:39:31 -0300

    folder fix

--- api/src/app.js
+++ api/src/app.js
@@ -1,23 +1,20 @@
-const express = require('express')
-const morgan = require('morgan')
-const getUser = require('../routes/user/get/getUser');
-const routes = require('./Routes/index.js')
+const express = require("express");
+const morgan = require("morgan");
+const getUser = require("../routes/user/get/getUser");
+const routes = require("./Routes/index.js");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Ruta GET /user
-app.use('/user', getUser);
+app.use("/user", getUser);
 //---------------
-
-
-app.get('/', (req, res)=>{
-    res.json('welcome to AutoCinema')
-})
-
-app.use('/', routes)
+app.get("/", (req, res) => {
+  res.json("welcome to AutoCinema");
+});
+app.use("/", routes);
 
 module.exports = app;
