e19ef8ed4e59ee35199f0510d200e0451d823a04
Wilmar De MelquisedecLisbet <carewaz23@hotmail.com>
2021-07-08 18:05:29 -0500

    change getMovie for getMovies

--- api/src/routes/Movies/movies.js
+++ api/src/routes/Movies/movies.js
@@ -4,7 +4,7 @@ const movieCtrl = require("../../controllers/movie.controller");
 const router = Router();
 
 router.get("/:id", movieCtrl.getMovieById);
-router.get("/", movieCtrl.getMovie);
+router.get("/", movieCtrl.getMovies);
 router.post("/", movieCtrl.postMovie);
 router.put("/:id", movieCtrl.putMovie);
 
