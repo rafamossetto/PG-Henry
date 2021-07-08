5770368701b9116e3041bf66a93b0821a7d41850
juancereceda <juanmcereceda@gmail.com>
2021-07-07 22:41:51 -0300

    authentication
    added new actions and passed to reducer to store the token provided in the backend to localStorage

--- client/src/reducer/reducer.js
+++ client/src/reducer/reducer.js
@@ -1,29 +1,40 @@
 import { GET_MOVIES_DETAIL, GET_MOVIE_LIST } from "../actions/movies";
 import { ORDER_USERS_BY_POINTS } from "../actions/points";
 import { GET_PRODUCTS, ADD_TOTAL, SUBSTRACT_TOTAL } from "../actions/products";
-import { GET_USERS } from "../actions/users";
+import { GET_USERS, SIGNUP, LOGIN } from "../actions/users";
 
 const initialState = {
   products: [],
   total: 0,
   movieDetail: {},
   users: [],
-  movieList: [],
+  token: getTokenLocalStorage(),
 };
+
+export function getTokenLocalStorage() {
+  const token = window.localStorage.getItem("token");
+  return token ? JSON.parse(token) : {};
+}
+
+function setTokenLocalStorage(token) {
+  window.localStorage.setItem("token", JSON.stringify(token));
+}
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
-          //movie list
+    //movie list
     case GET_MOVIE_LIST: {
       return {
         ...state,
         movieList: action.payload,
-      }
-    case GET_MOVIES_DETAIL:
+      };
+    }
+    case GET_MOVIES_DETAIL: {
       return {
         ...state,
         movieDetail: action.payload,
       };
+    }
     case GET_PRODUCTS: {
       return {
         ...state,
@@ -49,20 +60,37 @@ export default function reducer(state = initialState, action) {
         ...state,
         users: action.payload,
       };
+    }
     // Ordenar usuarios por cantidad de puntos asc/desc
     case ORDER_USERS_BY_POINTS: {
       //Si no hay payload, order desc
       if (!action.payload) {
         return {
           ...state,
-          users: [...state.users].sort((a, b) => a - b)
-        }
+          users: [...state.users].sort((a, b) => a - b),
+        };
       }
       return {
         ...state,
-        users: [...state.users].sort((a, b) => a + b)
-      }
+        users: [...state.users].sort((a, b) => a + b),
+      };
     }
+    //authentication
+    case LOGIN: {
+      setTokenLocalStorage(action.payload);
+      return {
+        ...state,
+        token: action.payload,
+      };
+    }
+    case SIGNUP: {
+      setTokenLocalStorage(action.payload);
+      return {
+        ...state,
+        token: action.payload,
+      };
+    }
+
     default: {
       return state;
     }
