4de72191c4b4a08e3d02480152b94445fb91c772
Gianfranco <70702029+Yiftleh@users.noreply.github.com>
2021-07-09 12:08:10 -0300

    Sign up working
    Now the sign up pushes the user in the DB and shows the error mesages or moves the user to the main page if the account is created.

--- client/src/actions/users.js
+++ client/src/actions/users.js
@@ -20,12 +20,19 @@ export function getUsers() {
 
 export function signUp(username, email, password) {
   return async function (dispatch) {
-    const token = await axios.post("http://localhost:3001/users/signup", {
-      username,
-      email,
-      password,
-    });
-    dispatch({ type: SIGNUP, payload: token.data.token });
+    try {
+      const token = await axios.post("http://localhost:3001/users/signup", {
+        username,
+        email,
+        password,
+      });
+      dispatch({ type: SIGNUP, payload: token.data.token });
+      return 'Account created'
+    } catch (error) {
+      if(error.response.status === 400){
+        return error.response.data
+      }
+    }
   };
 }
 
