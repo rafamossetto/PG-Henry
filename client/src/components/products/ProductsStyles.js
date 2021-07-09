c453f85e1d784c42c1a8c7f5e2e6062c03ce1027
Mauro Haidr <maurohaidr@gmail.com>
2021-07-09 12:19:28 -0300

    Changed Product sizes

--- client/src/components/products/ProductsStyles.js
+++ client/src/components/products/ProductsStyles.js
@@ -7,7 +7,7 @@ export const ProductsBox = styled.div`
 `;
 export const Container = styled.div`
     background-color: rgba(34, 40, 49, 1);
-    padding-top: 140px;
+    padding-top: 15%;
 `;
 export const MovieData = styled.div`
     display:flex;
@@ -16,8 +16,9 @@ export const MovieData = styled.div`
 `;
 export const MovieDetails = styled.div`
     position:static;
-    margin-left:20px;
-    min-width: 250px;
+    font-size: 24px;
+    margin-left:30px;
+    min-width: 500px;
     color:rgba(255, 255, 255, 1);
     font-family: Questrial;
     display:flex;
@@ -27,8 +28,8 @@ export const ParkingLot = styled.div`
     margin-top: 20px;
     display:flex;
     flex-direction: column;
-    width:520px;
-    height:210px;
+    width:780px;
+    height:315px;
     background-color: rgb(48, 71, 94);
 `;
 export const ParkingLine = styled.div`
@@ -36,7 +37,7 @@ export const ParkingLine = styled.div`
     flex-direction: row;
 `;
 export const Screen = styled.div`
-    margin-top: 5px;
+    margin: 5px 0 5px 0;
     display:flex;
     justify-content: center;
     border-radius: 6px;
@@ -44,9 +45,11 @@ export const Screen = styled.div`
     background-color:rgba(234, 84, 85, 1);
 `;
 export const Reference = styled.div`
-    margin-top: 5px;
+    margin-top: 10px;
     display:flex;
     justify-content:flex-start;
+    font-size: 20px;
+    color:rgba(255, 255, 255, 1);
 `;
 
 
@@ -57,39 +60,40 @@ export const RedText = styled.div`
     margin-left:10px;   
     color:rgb(234, 84, 85);
     font-family: Questrial;
-    font-size: 13px;
+    font-size: 30px;
 `;
 export const BuyBox = styled.div`
     display:flex;
     flex-direction: row;
     justify-content: flex-end;
     margin-right: 50px;
+    margin-bottom: 10px;
 `;
 export const BuyButton = styled.button`
-    margin-left:10px;
+    margin:0 30px 30px 10px;
     color:rgba(255, 255, 255, 1);
     font-family: Questrial;
-    font-size: 12px;
+    font-size: 30px;
     background-color:rgba(234, 84, 85, 1);
     border:none;
-    border-radius:5px;
-    width:50px;
-    height:35px;
+    border-radius:10px;
+    width:100px;
+    height:70px;
     &:hover{
         background-color:rgba(234, 84, 85, 0.7);
     }
 `;
 export const Total = styled.p`
     color:rgba(255, 255, 255, 1);
-    margin-top:8px;
+    margin-top:20px;
     font-family: Questrial;
-    font-size: 15px;
+    font-size: 30px;
 `
 export const StoredProducts = styled.p`
     color:rgba(255, 255, 255, 1);
-    margin-top:10px;
+    margin-top:30px;
     font-family: Questrial;
-    font-size: 13px;
+    font-size: 20px;
     margin-right:7px;
 `
 
