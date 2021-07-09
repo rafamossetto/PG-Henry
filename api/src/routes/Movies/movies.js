442bddd92299587b1d8440bfe178ae73b55b4fe5
juancereceda <juanmcereceda@gmail.com>
2021-07-09 13:03:47 -0300

    change movie from billboard to coming soon

--- api/src/routes/Movies/movies.js
+++ api/src/routes/Movies/movies.js
@@ -1,11 +1,20 @@
 const { Router } = require("express");
 const movieCtrl = require("../../controllers/movie.controller");
+const authentication = require("../../middlewares/authentication");
 
 const router = Router();
 
 router.get("/:id", movieCtrl.getMovieById);
 router.get("/", movieCtrl.getMovie);
-router.post("/", movieCtrl.postMovie);
-router.put("/:id", movieCtrl.putMovie);
+router.post(
+  "/",
+  [authentication.verifyToken, authentication.isAdmin],
+  movieCtrl.postMovie
+);
+router.put(
+  "/:id",
+  [authentication.verifyToken, authentication.isAdmin],
+  movieCtrl.putMovie
+);
 
 module.exports = router;
--- client/src/actions/movies.js
+++ client/src/actions/movies.js
@@ -1,41 +1,56 @@
+import axios from "axios";
+import { getTokenLocalStorage } from "../reducer/reducer";
 
-import axios from 'axios';
-export const GET_MOVIES_DETAIL = 'GET_MOVIES_BY_DETAIL';
-export const GET_MOVIE_LIST = 'GET_MOVIE_LIST';
+export const GET_MOVIES_DETAIL = "GET_MOVIES_BY_DETAIL";
+export const GET_MOVIE_LIST = "GET_MOVIE_LIST";
 
-export function getMovieById(id){
-    return function (dispatch) {
-        return axios.get(`http://localhost:3001/movies/${id}`)
-          .then(result => {
-            dispatch({
-                type: GET_MOVIES_DETAIL, 
-                payload:result.data
-              
-            }); 
-         }).catch(error=>{
-           if(error.response?.status!== 404) alert('something wrong');
-           dispatch({type:GET_MOVIES_DETAIL, payload:null})
-         })
-       }
-  }
+const config = {
+  headers: {
+    "Access-Control-Allow-Headers": "x-access-token",
+    "x-access-token": getTokenLocalStorage(),
+  },
+};
 
-export function getMovieList(){
-  return function (dispatch){
-    return axios.get('http://localhost:3001/movies')
-    .then(result => {
-      dispatch({
-        type: GET_MOVIE_LIST,
-        payload: result.data
+export function getMovieById(id) {
+  return function (dispatch) {
+    return axios
+      .get(`http://localhost:3001/movies/${id}`)
+      .then((result) => {
+        dispatch({
+          type: GET_MOVIES_DETAIL,
+          payload: result.data,
+        });
       })
-    })
-  }
+      .catch((error) => {
+        if (error.response?.status !== 404) alert("something wrong");
+        dispatch({ type: GET_MOVIES_DETAIL, payload: null });
+      });
+  };
 }
 
-export function clearMovie() { //se usa en el willunmount
-   return  { 
-              type: GET_MOVIES_DETAIL, // va a usar el mismo reducer de la acción getMovieById
-              payload: undefined
-          }; 
-     
-  }
-  
+export function getMovieList() {
+  return function (dispatch) {
+    return axios.get("http://localhost:3001/movies").then((result) => {
+      dispatch({
+        type: GET_MOVIE_LIST,
+        payload: result.data,
+      });
+    });
+  };
+}
+
+export function clearMovie() {
+  //se usa en el willunmount
+  return {
+    type: GET_MOVIES_DETAIL, // va a usar el mismo reducer de la acción getMovieById
+    payload: undefined,
+  };
+}
+
+export function postMovie(movie) {
+  axios.post("http://localhost:3001/movies", movie, config);
+}
+
+export function updateMovie(movie, id) {
+  axios.put(`http://localhost:3001/movies/${id}`, movie, config);
+}
