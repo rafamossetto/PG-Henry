80806f56b127d923d61497ac5bfa9fb9e3714286
Wilmar De MelquisedecLisbet <carewaz23@hotmail.com>
2021-07-10 00:31:56 -0500

    Use swal for alerts

--- client/src/components/login/LogIn.js
+++ client/src/components/login/LogIn.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { useHistory } from "react-router-dom";
+import React, {  useState } from "react";
+import swal from 'sweetalert';
+// import { useHistory } from "react-router-dom";
 import StyledContainer from "./LogInStyles";
 import { logIn } from "../../actions/users";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 function LogIn() {
   const dispatch = useDispatch();
@@ -19,12 +20,40 @@ function LogIn() {
   }
 
   async function handleLogInSubmit(e) {
-    e.preventDefault();
-    let message = await dispatch(logIn(logInState.name, logInState.password));
-    alert(message);
-    if (message === "Logged in succesfully") {
-      window.location.assign("http://localhost:3000/");
-    }
+    e.preventDefault(); 
+    if(logInState.name && logInState.password) {
+      let message = await dispatch(logIn(logInState.name, logInState.password));    
+      if (message === "Logged in succesfully") {
+        await swal(message, "Logged", "success", {
+          buttons: false,
+          timer: 2000
+        });
+        window.location.assign("http://localhost:3000/");
+      } else {
+        swal(message, "No Logged!", "error", {
+          buttons: false,
+          timer: 3000
+        });
+      }
+    } else {
+      !logInState.name && !logInState.password ? 
+        await swal("Required credentials!", "Add name or email and password", "warning", {
+          buttons: false,
+          timer: 3000,
+          className: "swal-overlay"
+        }) 
+      :
+      !logInState.name ? 
+        await swal("Required name or email!", "Add name or email", "warning", {
+          buttons: false,
+          timer: 3000
+        }) 
+      :
+      await swal("Required password!", "Add password", "warning", {
+        buttons: false,
+        timer: 3000
+      })
+    }    
   }
 
   return (
@@ -45,6 +74,7 @@ function LogIn() {
         <img
           className="google"
           src="https://res.cloudinary.com/juancereceda/image/upload/v1625856246/Screen_Shot_2021-07-09_at_15.43.47_jwdloi.png"
+          alt='Not found img'
         />
         <div className="btnContainer">
           <button type="submit" className="logIn">
