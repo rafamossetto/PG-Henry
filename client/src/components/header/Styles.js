5d4e48e58098b3ab789cca0fbd7edfe64d5cc190
juancereceda <juanmcereceda@gmail.com>
2021-07-10 14:27:42 -0300

    log out button and functionality

--- client/src/components/header/Styles.js
+++ client/src/components/header/Styles.js
@@ -10,37 +10,37 @@ import { TiShoppingCart as ShoppingCart } from "react-icons/ti";
 
 // ESTILOS DEL HEADER
 
-export const HeaderAlpha = styled.div`    
-    height: 25%;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    flex-wrap: wrap;
-    background-color: #222831;
+export const HeaderAlpha = styled.div`
+  height: 25%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  background-color: #222831;
+  font-family: Questrial;
 `;
 
 export const Head = styled.div`
-    background-color: #F05454;
-    width: 98%;
-    height: 67.5%;
-    margin-bottom: .3em;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    align-items: center;    
-    border-radius: .7em;
-    box-shadow: 5px 5px 5px rgba(0,0,0,0.7),
-                inset 2px 3px 5px rgba(0,0,0,0.3),
-                inset -2px -3px 5px rgba(0,0,0,0.5);
+  background-color: #f05454;
+  width: 98%;
+  height: 67.5%;
+  margin-bottom: 0.3em;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 0.7em;
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.7),
+    inset 2px 3px 5px rgba(0, 0, 0, 0.3), inset -2px -3px 5px rgba(0, 0, 0, 0.5);
 `;
 
 export const HeadInsider = styled.div`
   display: flex;
   flex-direction: row;
-  align-items: center;  
+  align-items: center;
 `;
 
 export const HeaderIcon = styled(Camera)`
@@ -49,7 +49,7 @@ export const HeaderIcon = styled(Camera)`
   padding: 1em;
   background-color: #222831;
   border-radius: 50%;
-  margin: .8em .8em .8em .5em;
+  margin: 0.8em 0.8em 0.8em 0.5em;
   color: #e8e8e8;
 `;
 
@@ -60,7 +60,7 @@ export const Socials = styled.a`
 `;
 export const Title = styled.h1`
   color: #e8e8e8;
-  margin-left: .6em;
+  margin-left: 0.6em;
 `;
 
 export const Face = styled(Fb)`
@@ -78,18 +78,31 @@ export const Insta = styled(Instagram)`
 // ESTILOS DEL NAVBAR
 
 export const NavBarAlpha = styled.div`
-    height: 27.5%;
-    width: 98%;
-    padding: 1.2em 0;
-    background-color: #30475E;
+  font-family: Questrial;
+  height: 27.5%;
+  width: 98%;
+  padding: 1.2em 0;
+  background-color: #30475e;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  border-radius: 0.7em;
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.7),
+    inset 2px 3px 5px rgba(0, 0, 0, 0.3), inset -2px -3px 5px rgba(0, 0, 0, 0.5);
+
+  .accountLogout {
     display: flex;
-    flex-direction: row;
-    justify-content: space-evenly;
     align-items: center;
-    border-radius: .7em;
-    box-shadow: 5px 5px 5px rgba(0,0,0,0.7),
-                inset 2px 3px 5px rgba(0,0,0,0.3),
-                inset -2px -3px 5px rgba(0,0,0,0.5);
+    .logout {
+      height: 24px;
+      margin-left: 20px;
+      &:hover {
+        opacity: 0.5;
+        transition: 0.2s;
+      }
+    }
+  }
 `;
 
 export const SignButton = styled.button`
@@ -112,93 +125,92 @@ export const Linked = styled(Link)`
 // ESTILOS SIGNFORM
 
 export const Signform = styled.div`
-    position: fixed;
-    top: 15%;
-    left: 30%;
-    height: 60%;
-    width: 30%;
-    padding: 10px;
-    background-color: #30475E;
-    padding-top: 25px;
-    box-shadow: 5px 5px 5px rgba(0,0,0,0.7),
-                inset 2px 3px 5px rgba(0,0,0,0.3),
-                inset -2px -3px 5px rgba(0,0,0,0.5);
+  position: fixed;
+  top: 15%;
+  left: 30%;
+  height: 60%;
+  width: 30%;
+  padding: 10px;
+  background-color: #30475e;
+  padding-top: 25px;
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.7),
+    inset 2px 3px 5px rgba(0, 0, 0, 0.3), inset -2px -3px 5px rgba(0, 0, 0, 0.5);
 `;
 
 export const FormForm = styled.form`
-    height: 100%;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const Inputs = styled.input`
-    font-size: 25px;
-    padding: 5px;
-    margin-top: -20px;
+  font-size: 25px;
+  padding: 5px;
+  margin-top: -20px;
 `;
 
 export const Buttons = styled.div`
-    width: 80%;
-    height: auto;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin-bottom: 20px;
+  width: 80%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
 `;
 
 export const TwoButtons = styled.div`
-    margin-top: 5px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
+  margin-top: 5px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
 `;
 
 export const GoogleButton = styled.button`
-    height: 40px;
-    width: 100%;
-    background-color: #E8E8E8;
-    border-radius: 5px;
-    font-size: 20px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-evenly;
-    align-items: center;
+  height: 40px;
+  width: 100%;
+  background-color: #e8e8e8;
+  border-radius: 5px;
+  font-size: 20px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
 `;
 
 export const LogButton = styled.button`
-    height: 40px;
-    width: 49%;
-    font-size: 20px;
-    background-color: #F05454;
-    color: #E8E8E8;
-    border-radius: 5px;
+  height: 40px;
+  width: 49%;
+  font-size: 20px;
+  background-color: #f05454;
+  color: #e8e8e8;
+  border-radius: 5px;
 `;
 
 export const SigButton = styled.button`
-    height: 40px;
-    width: 49%;
-    font-size: 20px;
-    background-color: #E8E8E8;
-    border-radius: 5px;
+  height: 40px;
+  width: 49%;
+  font-size: 20px;
+  background-color: #e8e8e8;
+  border-radius: 5px;
 `;
 
 export const Linking = styled(Link)`
-    color: black;
-    text-decoration: none;
+  color: black;
+  text-decoration: none;
 `;
 
 export const CloseButton = styled.button`
-    height: 25px;
-    width: 25px;
-    background-color: #F05454;
-    color: #E8E8E8;
-    text-align: center;
-    align-self: flex-end;
-    margin-top: -15px;
+  height: 25px;
+  width: 25px;
+  background-color: #f05454;
+  color: #e8e8e8;
+  text-align: center;
+  align-self: flex-end;
+  margin-top: -15px;
 `;
