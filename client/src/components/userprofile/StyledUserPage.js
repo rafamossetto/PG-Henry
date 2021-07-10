0b3fbb9701daacb182f053ec7a965e236f18f73b
juancereceda <juanmcereceda@gmail.com>
2021-07-10 16:48:51 -0300

    some validations and new messages in admin page and user profile route

---
+++ client/src/components/userprofile/StyledUserPage.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const div = styled.div`
+  font-family: Questrial;
+  color: #e8e8e8;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding-top: 5%;
+
+  .errorCnt {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .sadFace {
+      width: 150px;
+    }
+  }
+`;
+
+export default div;
