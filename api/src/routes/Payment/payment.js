c08db614d0b69346abc6ab6ef16fd60059df8c5a
juancereceda <juanmcereceda@gmail.com>
2021-07-14 13:46:48 -0300

    payment controllers to update status of an order and route that responds with a payment link

--- api/src/routes/Payment/payment.js
+++ api/src/routes/Payment/payment.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 const paymentCtrl = require("../../controllers/payment.controller");
+const authentication = require("../../middlewares/authentication");
 
-router.post("/", paymentCtrl.processPayment);
-
+router.post("/", authentication.verifyToken, paymentCtrl.processPayment);
 module.exports = router;
