0183a203c60ffea7a0bcb7b08d2c3c905f782c9c
RuthCodina <ruth0066@hotmail.com>
2021-07-15 12:05:06 -0500

    cd

--- client/src/components/GenreFilter/GenreFilter.js
+++ client/src/components/GenreFilter/GenreFilter.js
@@ -31,28 +31,7 @@ function GenreFilter(){
      }
 
 return(
-    <>
-    <input name='genere' type='text' onChange={handleGenre} value={input.inputGenre} placeholder='¿qué genero de peli, quieres ver?'/>
-    <button onClick={handleSubmitGenre}>Search</button>
-    // {!input.name? <BillboardCard/>: 
-        {moviesGenre? moviesGenre.map((el)=>(
-        <div>
-           <h2><Link to={`/movies/${el._id}`}>{el.title}</Link></h2>
-            <Link to={`/movies/${el._id}`}><img src={el.poster} alt={`Poster of the movie "${el.title}"`}/></Link>
-            <p>{el.description}</p>
-            <h4>Gender: <span>{el.genre}</span></h4>
-            <h5>Cast: <span>{el.cast}</span></h5>
-            <h6>IMDB Rating: <span>{el.rating}</span></h6>
-        </div>
-    )): <div className="errorCnt">
-          <img align-text='center'
-             className="sadFace"
-            src="https://res.cloudinary.com/juancereceda/image/upload/v1625945361/sad-face-in-rounded-square_q7qmr7.png"
-            alt="404"
-          />
-          <h1 className="errorMsg">Upps! Currently, we don't have that genre, meanwhile explore others</h1>
-       </div>}
-    </>
+   
    ) 
 }
 
