cd2b5fdaf870a7dd30e6a6130840ac425f5cc8a8
CelenyAndrea <77702240+CelenyAndrea@users.noreply.github.com>
2021-07-15 10:55:40 -0500

    blockUser

--- client/src/components/adminpage/users/index.js
+++ client/src/components/adminpage/users/index.js
@@ -1,53 +1,114 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import styled from 'styled-components';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import StyledDiv from './userStyles';
+import { getUsers, updateUser } from '../../../actions/users';
+import swal from "sweetalert";
 
 const Users = () => {
- 
+    const dispatch = useDispatch();
     const users = useSelector(state => state.users);
+    // const [user, setUser] = useState({
+    //     username: "",
+    //     email: "",
+    //     password: "",
+    //     isAdmin: false,
+    //     bookings: [],
+    //     banned: false,
+    // })
 
-    const StyledDiv = styled.div`
-    margin-left: .8em;
-    display: flex;
-    flex-direction: column;
-    
-    h4 {
-        font-weight: 600;
-        font-size: 1.5em;
-    }
+    useEffect(() => {
+        dispatch(getUsers());
+    }, []);
+   
+    const handleSubmit = (user, e) => {
+        e.preventDefault();
 
-    .userDiv {
-        display: flex;
-        justify-content: space-between;
-        width: 50%;
-        align-items: center;
-        color: #ffffff;        
+        const obj = {
+            username: user.username,
+            email: user.email,
+            password: user.password,
+            isAdmin: user.isAdmin,
+            bookings: user.bookings,
+            banned: user.banned,
+        }
+
+        if(obj.banned === 'UserBlock') {
+            swal({
+                title: "Are you sure?",
+                text: `The Blocked to ${user.username}`,
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+            })
+            .then((willBlocked) => {
+                if (willBlocked) {
+                  swal("Blocked User!", {
+                    icon: "success",
+                  });
+                } else {
+                  swal("Your user is safe!");
+                }
+            });
+        }
+
+        dispatch(updateUser)
+        ({
+            ...user,
+            banned: !user.banned,
+        },
+        user._id
+        )
     }
-    .userButton {
-        border-radius: 15%;
-        height: 3em;
-        
-    }
-    `;
 
     return (
         <>
         {
             window.localStorage.token && users.length ?
                 <StyledDiv>
-                    <h1>Users registrates</h1>
-                    {users &&
-                        users.map(user => {
-                            return <div className='userDiv'>
-                                <h4>{user.username} 💨</h4>
-                                <button className='userButton'>{user.isAdmin ? 'ChangeToUser' : 'ChangeToAdmin'}</button>
-                            </div>
-                        })}
+                    <h1>Users registrates</h1>     
+                    <table>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <h2>Username</h2>
+                                <h2>Email</h2>
+                                <h2>Block User</h2>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {users &&
+                            users.map(user => (
+                                <tr>
+                                    <td>
+                                    </td>
+                                    <td>{user.username}</td>
+                                    <td>{user.email}</td>
+                                    <button
+                                    className='userButton'
+                                    >{user.isAdmin ? 'ChangeToUser' : 'ChangeToAdmin'}
+                                    </button>
+                                    <button
+                                    key={user._id}
+                                    className='userButton'
+                                    onClick={(e) => handleSubmit(user,e)}
+                                    >{user.banned ? 'UserBlock' : 'Disable'}
+                                    </button>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
                 </StyledDiv>
             :
-                <StyledDiv>
-                    <h1>Sólo personal autorizado...</h1>
-                </StyledDiv>
+            <StyledDiv>
+                <div className="errorCnt">
+                    <img
+                        className="sadFace"
+                        src="https://res.cloudinary.com/juancereceda/image/upload/v1625945361/sad-face-in-rounded-square_q7qmr7.png"
+                        alt="404"
+                    />
+                    <h1 className="errorMsg">Sorry! We've nothing for you here</h1>
+                </div>
+            </StyledDiv>
         }
         </>
     )
