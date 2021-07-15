9d230b6be00ae7f6b43f39ddacfa208a08ba726c
Mauro Haidr <maurohaidr@gmail.com>
2021-07-15 12:40:03 -0300

    Changed postMovie controller, to add movies with show dates

--- api/src/controllers/movie.controller.js
+++ api/src/controllers/movie.controller.js
@@ -21,7 +21,54 @@ const getMovies = async (req, res) => {
 
 const postMovie = async (req, res) => {
   try {
-    const { title, date, poster, description, genre, onBillboard, shows, cast, trailer, rated, runtime, director } = req.body;
+    const { dates, functionDays, times, date, price, title, poster, description, genre, onBillboard, cast, trailer, rated, runtime, director } = req.body;
+    
+    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    const shows = [];
+    const startDate = new Date (dates[0].year, dates[0].month-1, dates[0].day);
+    const finishDate = new Date (dates[1].year, dates[1].month-1, dates[1].day);
+    for ( i = startDate; i <= finishDate; i.setDate(i.getDate() + 1)){
+        let day = days[i.getDay()];
+        if(functionDays.includes(day)){
+          let date = new Date(i)
+          let time = times.map(
+            e => e = {[e] : [
+            {"slot":"A1","ocuppied":false},
+            {"slot":"A2","ocuppied":false},
+            {"slot":"A3","ocuppied":false},
+            {"slot":"A4","ocuppied":false},
+            {"slot":"A5","ocuppied":false},
+            {"slot":"A6","ocuppied":false},
+            {"slot":"A7","ocuppied":false},
+            {"slot":"A8","ocuppied":false},
+            {"slot":"A9","ocuppied":false},
+            {"slot":"A10","ocuppied":false},
+            {"slot":"B1","ocuppied":false},
+            {"slot":"B2","ocuppied":false},
+            {"slot":"B3","ocuppied":false},
+            {"slot":"B4","ocuppied":false},
+            {"slot":"B5","ocuppied":false},
+            {"slot":"B6","ocuppied":false},
+            {"slot":"B7","ocuppied":false},
+            {"slot":"B8","ocuppied":false},
+            {"slot":"B9","ocuppied":false},
+            {"slot":"B10","ocuppied":false},
+            {"slot":"C1","ocuppied":false},
+            {"slot":"C2","ocuppied":false},
+            {"slot":"C3","ocuppied":false},
+            {"slot":"C4","ocuppied":false},
+            {"slot":"C5","ocuppied":false},
+            {"slot":"C6","ocuppied":false},
+            {"slot":"C7","ocuppied":false},
+            {"slot":"C8","ocuppied":false},
+            {"slot":"C9","ocuppied":false},
+            {"slot":"C10","ocuppied":false}
+          ]}
+        )
+          shows.push({date, day, price, time})
+        }
+    }
+
     const movie = await new Movie({
       title,
       date,
@@ -37,7 +84,6 @@ const postMovie = async (req, res) => {
       director  
   });
   const movieSaved = await movie.save();
-  console.log(movie);
   res.send(movieSaved)
   } catch (error) {
     res.status(400).send(error)
