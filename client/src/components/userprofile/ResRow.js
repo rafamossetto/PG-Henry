286ad53ea0108b86ea4d6a4da214592e5e2b5bc7
Juan Roberts <juancroberts31@gmail.com>
2021-07-15 21:32:59 -0300

    Paymnent options added to Bookings

--- client/src/components/userprofile/ResRow.js
+++ client/src/components/userprofile/ResRow.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export default function ResRow ({title, date, time, lot, extras, status}) {
+export default function ResRow ({title, date, time, lot, extras, status, url}) {
+    function handleClick (e) {
+        e.preventDefault();
+
+        window.open(url);
+    }
+
+    const But = styled.button`
+        margin-top: 5px;
+        border-radius: 5px;
+        background-color: #E8E8E8;
+        border: none;
+        box-shadow: inset 2px 3px 5px rgba(0, 0, 0, 0.3), inset -2px -3px 5px rgba(0, 0, 0, 0.5);
+        &:focus {
+            box-shadow: inset -2px -3px 5px rgba(0, 0, 0, 0.3), inset 2px 3px 5px rgba(0, 0, 0, 0.5);
+        }
+    `;
+
     return (
         <tr>
             <td>{title}</td>
@@ -10,6 +28,7 @@ export default function ResRow ({title, date, time, lot, extras, status}) {
             <td>{lot}</td>
             <td>{extras.join(", ")}</td>
             <td>{status}</td>
+            {status === "processing" ? <But onClick={(e) => handleClick(e)} >Pay</But> : null}
         </tr>
     )
 }
