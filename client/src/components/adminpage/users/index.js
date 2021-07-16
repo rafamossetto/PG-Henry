dafb2dc5248449cc2e11f5c2cc454435850ef8ed
juancereceda <juanmcereceda@gmail.com>
2021-07-15 23:13:37 -0300

    fixing bug that restores password

--- client/src/components/adminpage/users/index.js
+++ client/src/components/adminpage/users/index.js
@@ -1,81 +1,77 @@
-
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { updateUser, getUsers } from '../../../actions/users';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { updateUser, getUsers } from "../../../actions/users";
 import swal from "sweetalert";
-import StyledDiv from './userStyles';
+import StyledDiv from "./userStyles";
 
-const Users = () => { 
+const Users = () => {
+  const users = useSelector((state) => state.users);
+  const dispatch = useDispatch();
 
-    
-    const users = useSelector(state => state.users)
-    const dispatch = useDispatch()
-
-    const handleClick = (user, e) => {
-        e.preventDefault()
-        dispatch(getUsers());
-        console.log('Admin: ', user.isAdmin)
-        dispatch(updateUser(
-            {
-                ...user, 
-                isAdmin: !user.isAdmin
-            },
-            user._id
-        ))
-        dispatch(getUsers());
-    }
-    return (
-        <>
+  const handleClick = (user, e) => {
+    e.preventDefault();
+    dispatch(getUsers());
+    console.log("Admin: ", user.isAdmin);
+    dispatch(
+      updateUser(
         {
-            window.localStorage.token && users.length ?
-                <StyledDiv>
-                    <h1>Users registrates</h1>     
-                    <table>
-                        <thead>
-                            <tr>
-                                <th></th>
-                                <h2>Username</h2>
-                                <h2>Email</h2>
-                                <h2>Block User</h2>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {users &&
-                            users.map(user => (
-                                <tr key={user._id}>
-                                    <td>
-                                    </td>
-                                    <td>{user.username}</td>
-                                    <td>{user.email}</td>
-                                    <button
-                                    className='userButton'
-                                    onClick={(e) => handleClick(user, e)}
-                                    >{user.isAdmin ? 'ChangeToUser' : 'ChangeToAdmin'}
-                                    </button>
-                                    <button
-                                    className='userButton'
-                                    
-                                    >{user.banned ? 'UserBlock' : 'Disable'}
-                                    </button>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </StyledDiv>
-            :
-            <StyledDiv>
-                <div className="errorCnt">
-                    <img
-                        className="sadFace"
-                        src="https://res.cloudinary.com/juancereceda/image/upload/v1625945361/sad-face-in-rounded-square_q7qmr7.png"
-                        alt="404"
-                    />
-                    <h1 className="errorMsg">Sorry! We've nothing for you here</h1>
-                </div>
-            </StyledDiv>
-        }
-        </>
-    )
-}
+          ...user,
+          isAdmin: !user.isAdmin,
+        },
+        user._id
+      )
+    );
+    dispatch(getUsers());
+  };
+  return (
+    <>
+      {window.localStorage.token && users.length ? (
+        <StyledDiv>
+          <h1>Users registrates</h1>
+          <table>
+            <thead>
+              <tr>
+                <th></th>
+                <h2>Username</h2>
+                <h2>Email</h2>
+                <h2>Block User</h2>
+              </tr>
+            </thead>
+            <tbody>
+              {users &&
+                users.map((user) => (
+                  <tr key={user._id}>
+                    <td></td>
+                    <td>{user.username}</td>
+                    <td>{user.email}</td>
+                    <button
+                      className="userButton"
+                      onClick={(e) => handleClick(user, e)}
+                    >
+                      {user.isAdmin ? "ChangeToUser" : "ChangeToAdmin"}
+                    </button>
+                    <button className="userButton">
+                      {user.banned ? "UserBlock" : "Disable"}
+                    </button>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </StyledDiv>
+      ) : (
+        <StyledDiv>
+          <div className="errorCnt">
+            <img
+              className="sadFace"
+              src="https://res.cloudinary.com/juancereceda/image/upload/v1625945361/sad-face-in-rounded-square_q7qmr7.png"
+              alt="404"
+            />
+            <h1 className="errorMsg">Sorry! We've nothing for you here</h1>
+          </div>
+        </StyledDiv>
+      )}
+    </>
+  );
+};
 
 export default Users;
