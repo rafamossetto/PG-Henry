569d071eeef994266fcab1f6963a3cf0255c27e2
Mauro Haidr <maurohaidr@gmail.com>
2021-07-18 16:50:03 -0300

    Success page now redirects to /profile after 3 secs

--- client/src/components/success/Success.js
+++ client/src/components/success/Success.js
@@ -1,25 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {updateStatus} from '../../actions/products'
-
+import SuccessMsg from './SuccessStyles'
 const Success = (props) => {
 
     const queryString = props.location.search.substr(1)
     let queryArr = queryString.split('&')
     let queryArr2 = queryArr.map( e => e.split('=') )
+
     const status = queryArr2.filter(e => e[0] === 'collection_status')[0][1]
     const preference_id = queryArr2.filter(e => e[0] === 'preference_id')[0][1]
 
-    console.log(status)
-    console.log(preference_id)
-    /* const UserId = props.match.params.id */
+    const [counter, setCounter] = useState(3)
 
     useEffect(() => {
         updateStatus({status, preference_id})
-    }, [status, preference_id])
+        let intervalo = null;
+        intervalo = setInterval(() => {  
+            if(counter > 0) setCounter(counter - 1)
+            if(counter === 1){
+                clearInterval()  
+                window.location.assign('http://localhost:3000/profile')
+            }
+        }, 1000)            
+    }, [status, preference_id, counter])
     return (
-        <div>
-            <h3>Payment Aproved</h3>
-        </div>
+        <SuccessMsg>
+            <div className="successBox">
+                <h2>Payment Aproved</h2>
+                <h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Redirecting in {counter}...</h3>
+            </div>
+        </SuccessMsg>
     )
 }
 
